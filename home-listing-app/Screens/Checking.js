91e5d47f4b25d1260842eedce0c67af02c6dc6ea
Ibrahim <ibrahimfayyaz26@gmail.github.com>
2021-02-17 02:15:44 +0500

    Checking

---
+++ home-listing-app/Screens/Checking.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+
+const Checking = () => {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+};
+
+styles = StyleSheet.create({});
+
+export default Checking;
